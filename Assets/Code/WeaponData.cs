a81fbb712217ab8bc48aae9b8ace9168bacc3800
IlsanGaming <whrkdgus34@gmail.com>
2024-12-15 16:09:40 +0900

    웨폰데이터

--- Assets/Code/WeaponData.cs
+++ Assets/Code/WeaponData.cs
@@ -7,8 +7,14 @@ using UnityEngine;
 public class WeaponData : ScriptableObject
 {
     //패시브 스탯
+<<<<<<< Updated upstream
     [Header("#레벨별 스폰지연시간")]
     public float[] speed;
     [Header("#레벨별 공격력")]
+=======
+    [Header("#투사체 속도(방향)")]
+    public float[] speed;
+    [Header("#관통력")]
+>>>>>>> Stashed changes
     public float[] count;
 }
